d7516a032352065dcb4f48235df8237d52fda72c
Duncan Cross <duncan@azuki.io>
2023-08-28 19:40:56 +0100

    Text tool added

--- src/main.ts
+++ src/main.ts
@@ -395,6 +395,183 @@ function floodFill(
   }
 }
 
+const codepageChars = [
+  0x00,
+  0x263A,
+  0x263B,
+  0x2665,
+  0x2666,
+  0x2663,
+  0x2660,
+  0x2022,
+  0x25D8,
+  0x25CB,
+  0x25D9,
+  0x2642,
+  0x2640,
+  0x266A,
+  0x266B,
+  0x263C,
+
+  0x25BA,
+  0x25C4,
+  0x2195,
+  0x203C,
+  0x00B6,
+  0x00A7,
+  0x25AC,
+  0x21A8,
+  0x2191,
+  0x2193,
+  0x2192,
+  0x2190,
+  0x221F,
+  0x2194,
+  0x25B2,
+  0x25BC,
+
+  ...Array.from({ length: 0xFF - 0x20 }, (_, i) => 0x20 + i),
+  0x2302,
+
+  0x00C7,
+  0x00FC,
+  0x00E9,
+  0x00E2,
+  0x00E4,
+  0x00E0,
+  0x00E5,
+  0x00E7,
+  0x00EA,
+  0x00EB,
+  0x00E8,
+  0x00EF,
+  0x00EE,
+  0x00EC,
+  0x00C4,
+  0x00C5,
+
+  0x00C9,
+  0x00E6,
+  0x00C6,
+  0x00F4,
+  0x00F6,
+  0x00F2,
+  0x00FB,
+  0x00F9,
+  0x00FF,
+  0x00D6,
+  0x00DC,
+  0x00A2,
+  0x00A3,
+  0x00A5,
+  0x20A7,
+  0x0192,
+
+  0x00E1,
+  0x00ED,
+  0x00F3,
+  0x00FA,
+  0x00F1,
+  0x00D1,
+  0x00AA,
+  0x00BA,
+  0x00BF,
+  0x2310,
+  0x00AC,
+  0x00BD,
+  0x00BC,
+  0x00A1,
+  0x00AB,
+  0x00BB,
+
+  0x2591,
+  0x2592,
+  0x2593,
+  0x2502,
+  0x2524,
+  0x2561,
+  0x2562,
+  0x2556,
+  0x2555,
+  0x2563,
+  0x2551,
+  0x2557,
+  0x255D,
+  0x255C,
+  0x255B,
+  0x2510,
+
+  0x2514,
+  0x2534,
+  0x252C,
+  0x251C,
+  0x2500,
+  0x253C,
+  0x255E,
+  0x255F,
+  0x255A,
+  0x2554,
+  0x2569,
+  0x2566,
+  0x2560,
+  0x2550,
+  0x256C,
+  0x2567,
+
+  0x2568,
+  0x2564,
+  0x2565,
+  0x2559,
+  0x2558,
+  0x2552,
+  0x2553,
+  0x256B,
+  0x256A,
+  0x2518,
+  0x250C,
+  0x2588,
+  0x2584,
+  0x258C,
+  0x2590,
+  0x2580,
+
+  0x03B1,
+  0x00DF,
+  0x0393,
+  0x03C0,
+  0x03A3,
+  0x03C3,
+  0x00B5,
+  0x03C4,
+  0x03A6,
+  0x0398,
+  0x03A9,
+  0x03B4,
+  0x221E,
+  0x03C6,
+  0x03B5,
+  0x2229,
+
+  0x2261,
+  0x00B1,
+  0x2265,
+  0x2264,
+  0x2320,
+  0x2321,
+  0x00F7,
+  0x2248,
+  0x00B0,
+  0x2219,
+  0x00B7,
+  0x221A,
+  0x207F,
+  0x00B2,
+  0x25A0,
+  0x00A0,
+];
+
+const codepageMap = new Map<string, number>(codepageChars.map((v, i) => [String.fromCodePoint(v), i]));
+
 async function main() {
   const req = await reqPromise;
   const b = await req.blob();
@@ -482,7 +659,16 @@ async function main() {
     e.preventDefault();
   };
   let currentChars = [219, 0, 0];
-  const tools: {[toolName: string]: () => void} = {
+  let cursorX = 0, cursorY = 0;
+  const cursorElement = document.querySelector('.cursor') as HTMLElement;
+  const setCursorPosition = (x: number, y: number) => {
+    cursorElement.style.left = `${x * 8}px`;
+    cursorElement.style.top = `${y * 16}px`;
+    cursorX = x;
+    cursorY = y;
+  };
+  const editorBlock = document.querySelector('#editor-block') as HTMLElement;
+  const tools: {[toolName: string]: () => (() => void) | void} = {
     freehand: () => {
       canvas.onpointerdown = e => {
         if (e.pointerType === 'mouse') {
@@ -827,13 +1013,121 @@ async function main() {
         }
       };
     },
+    text: () => {
+      editorBlock.focus();
+      canvas.onpointerdown = e => {
+        if (e.pointerType === 'mouse') {
+          const { button, pointerId } = e;
+          if (button !== 0) return;
+          e.preventDefault();
+          const rect = canvas.getBoundingClientRect();
+          const x = Math.floor((e.clientX - rect.x) * 80 / rect.width);
+          const y = Math.floor((e.clientY - rect.y) * 25 / rect.height);
+          setCursorPosition(x, y);
+          editorBlock.focus();
+        }
+      };
+      const type = (c: number) => {
+        screen.setChar(cursorX, cursorY, c, colors[0], colors[2], flags);
+        ctx!.globalCompositeOperation = 'copy';
+        ctx!.drawImage(screen.canvas, 0, 0);
+        if (cursorX === (SCREEN_WIDTH-1)) {
+          if (cursorY !== (SCREEN_HEIGHT-1)) {
+            setCursorPosition(0, cursorY + 1);
+          }
+        }
+        else {
+          setCursorPosition(cursorX+1, cursorY);
+        }
+      };      
+      function onkeydown(e: KeyboardEvent) {
+        console.log(e.key);
+        if (e.key === 'Tab') return;
+        e.preventDefault();
+        e.stopPropagation();
+        switch (e.key) {
+          case 'ArrowDown': {
+            setCursorPosition(cursorX, Math.min(cursorY + 1, SCREEN_HEIGHT-1));
+            return;
+          }
+          case 'ArrowUp': {
+            setCursorPosition(cursorX, Math.max(cursorY - 1, 0));
+            return;
+          }
+          case 'ArrowLeft': {
+            if (cursorX === 0) {
+              if (cursorY !== 0) {
+                setCursorPosition(SCREEN_WIDTH-1, cursorY-1);
+              }
+            }
+            else {
+              setCursorPosition(cursorX-1, cursorY);
+            }
+            return;
+          }
+          case 'ArrowRight': {
+            if (cursorX === (SCREEN_WIDTH-1)) {
+              if (cursorY !== (SCREEN_HEIGHT-1)) {
+                setCursorPosition(0, cursorY + 1);
+              }
+            }
+            else {
+              setCursorPosition(cursorX+1, cursorY);
+            }
+            return;
+          }
+          case 'Enter': {
+            setCursorPosition(0, Math.min(SCREEN_HEIGHT-1, cursorY+1));
+            return;
+          }
+          case 'End': case 'PageDown': {
+            setCursorPosition(cursorX, SCREEN_HEIGHT-1);
+            return;
+          }
+          case 'Home': case 'PageUp': {
+            setCursorPosition(cursorX, 0);
+            return;
+          }
+          case 'Backspace': {
+            if (cursorX === 0) {
+              if (cursorY !== 0) {
+                setCursorPosition(SCREEN_WIDTH-1, cursorY-1);
+              }
+            }
+            else {
+              setCursorPosition(cursorX-1, cursorY);
+            }
+            screen.setChar(cursorX, cursorY, 0, colors[0], colors[2], flags);
+            ctx!.globalCompositeOperation = 'copy';
+            ctx!.drawImage(screen.canvas, 0, 0);
+            return;
+          }
+          case 'Delete': {
+            screen.setChar(cursorX, cursorY, 0, colors[0], colors[2], flags);
+            ctx!.globalCompositeOperation = 'copy';
+            ctx!.drawImage(screen.canvas, 0, 0);
+            return;
+          }
+        }
+        const charCode = codepageMap.get(e.key);
+        if (typeof charCode === 'number') {
+          type(charCode);
+        }
+      }
+      editorBlock.addEventListener('keydown', onkeydown);
+      return () => {
+        editorBlock.removeEventListener('keydown', onkeydown);
+      };
+    },
   };
-  tools.freehand();
+  let unloadTool: (() => void) | void;
+  unloadTool = tools.freehand();
   const toolSelector = document.getElementById('tool-selector') as HTMLSelectElement;
   let selectedTool = toolSelector.value;
   document.body.classList.add('tool-' + selectedTool);
   toolSelector.onchange = (e) => {
-    tools[toolSelector.value]();
+    if (unloadTool) unloadTool();
+    unloadTool = tools[toolSelector.value]();
     document.body.classList.remove('tool-' + selectedTool);
     selectedTool = toolSelector.value;
     document.body.classList.add('tool-' + selectedTool);
